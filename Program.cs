4e1e65d26782a60b4dfc4e2e20fe30fad98ff378
biaaimi <biagioni.aimi@gmail.com>
2021-08-27 17:49:40 -0300

    resultado musica

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace musica_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.ForegroundColor=ConsoleColor.DarkRed;
+Console.WriteLine("Me encontra CHARLIE BROWN JR ");
+Console.ResetColor();
+Console.WriteLine("Hoje eu vou sair para encontrar o amor");
+Console.WriteLine("Espero a tanto tempo, ainda não rolou");
+Console.WriteLine("O vento diz que é hoje em meio à multidão");
+Console.WriteLine("Que eu vou encontrar a dona do meu coração.");
+Console.WriteLine("Aí sempre");
+Console.WriteLine("Sorrir, chorar e ter alguém pra compartilhar sempre");
+Console.WriteLine("Viver para alguém que me ama e dividir sempre");
+Console.WriteLine("Felicidade e amor.");
+Console.ForegroundColor=ConsoleColor.DarkRed;
+Console.WriteLine("Então me encontra ou deixa eu te encontrar");
+Console.WriteLine("Me encontra ou deixa eu te encontrar [x3]");
+Console.ResetColor();
+Console.WriteLine("Eu não conheço todas as flores,");
+Console.WriteLine("Mas vou mandar todas que eu puder");
+Console.WriteLine("Vivemos tempos de loucos amores");
+Console.WriteLine("Só é feliz quem sabe o que quer");
+Console.ForegroundColor=ConsoleColor.DarkRed;
+Console.WriteLine("Me encontra ou deixa eu te encontrar [x3]");
+Console.ResetColor();
+Console.WriteLine("Fico pensando onde está você");
+Console.WriteLine("E se você estaria pensando em me encontrar");
+Console.WriteLine("Como sou, onde estou e onde quero chegar");
+Console.WriteLine("Como sou, como é que vai ser e onde vou te levar");
+Console.WriteLine("Mas se você me ver pode acenar pra mim");
+Console.WriteLine("Já pensou que louco te encontrar assim?");
+Console.WriteLine("Eu vou na boa, eu vou na fé, eu sei que vou te encontrar");
+Console.WriteLine("E quando eu te encontrar nós vamos comemorar");
+Console.ForegroundColor=ConsoleColor.DarkRed;
+Console.WriteLine("Me encontra ou deixa eu te encontrar [x4]");
+Console.WriteLine("Te encontrar");
+Console.ResetColor();
+
+            
+        }
+     }
+}
